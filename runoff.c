8a11797f72d619e49e725c0a2dddd96d76fc5dc7
luckyB342 <luckyB342@users.noreply.github.com>
2020-07-22 22:31:18 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -201,7 +201,7 @@ bool is_tie(int min)
     int ties = 0;
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes == min && candidates[i].eliminated == false)
+        if (candidates[i].eliminated == false)
         {
             ties++;
             return true;
