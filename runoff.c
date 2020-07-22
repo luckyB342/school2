d4c2435297b5b314455b206fdfa49579fad71d3f
luckyB342 <luckyB342@users.noreply.github.com>
2020-07-22 22:39:19 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -199,15 +199,23 @@ int find_min(void)
 bool is_tie(int min)
 {
     int ties = 0;
+    bool istie = false;
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].eliminated == false)
+        if (candidates[i].votes == min && candidates[i].eliminated == false)
         {
             ties++;
-            return true;
+            int elim_cands = candidate_count - candidate_count;
+
+            if (ties == candidate_count || ties == elim_cands)
+            {
+                istie = true;
+                break;
+            }
         }
+
     }
-    return false;
+    return istie;
 }
 
 // Eliminate the candidate (or candidiates) in last place
