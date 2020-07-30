fb01453be87829a8ed77b8ec6bd1b1860e065694
luckyB342 <luckyB342@users.noreply.github.com>
2020-07-30 21:27:27 +0000

    automated commit by submit50

--- cash.py
+++ cash.py
@@ -6,7 +6,7 @@ user_input = -1
 while user_input < 0:
     user_input = get_float("How much change is owed: ")
 
-c = int(round(user_input * 100))
+c = round(user_input * 100)
 
 #Divide by 25 to get number of quarters and save by the remainder
 qc = int(c / 25)
@@ -14,14 +14,13 @@ r = int(c % 25)
 
 # Divide by 10 to get number of dime to save remainder
 dc = int(r / 10)
-r = r % 10
+r = int(r % 10)
 
 #Divide by 5 to get number of nickel to save remainder
 nc = int(r / 5)
 pc = int(r % 5)
 
 # Get sum of count
-coins = int(qc + dc + nc + pc)
-
+coins = round(qc + dc + nc + pc)
 
 print(f"The last number of coins is {coins}")
